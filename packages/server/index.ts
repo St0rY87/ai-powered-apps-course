64263b0f6cb35488e065140291d1e51967304ece
Dmitriy <mr.dima337@gmail.com>
2025-10-14 19:28:56 +0300

    refactor: create chat.controller.ts

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,7 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import dotenv, { parse } from 'dotenv';
-import z from 'zod';
-import { chatService } from './services/chat.services';
-
+import dotenv from 'dotenv';
+import { chatController } from './controllers/chat.controller';
 // Load environment variables from .env file
 dotenv.config();
 
@@ -20,30 +18,9 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello' });
 });
 
-const chatSchema = z.object({
-   prompt: z
-      .string()
-      .min(1, 'Prompt is required')
-      .max(1000, 'Prompt is too long (max 1000 characters)'),
-   conversationId: z.string().uuid(),
-});
 
-app.post('/api/chat', async (req: Request, res: Response) => {
-   const parseResult = chatSchema.safeParse(req.body);
-   if (!parseResult.success) {
-      res.status(400).json(parseResult.error.format());
-      return;
-   }
 
-   try {
-      const { prompt, conversationId } = req.body;
-      const response = await chatService.sendMessage(prompt, conversationId)
-
-      res.json({ message: response.message });
-   } catch (error) {
-      res.status(500).json({ error: 'Failed to generate a response.' });
-   }
-});
+app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
