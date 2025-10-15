ebb63b9bcd3f5da9937ec0f5599df8e0c0803f46
Dmitriy <mr.dima337@gmail.com>
2025-10-15 21:52:18 +0300

    feat: implement request to server via axios

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,24 +1,32 @@
+import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
+import { useRef } from 'react';
 
 type FormData = {
    prompt: string;
 };
 
 const ChatBot = () => {
-   const { register, handleSubmit, reset, formState } = useForm<FormData>();
-   const onSubmit = (data: FormData) => {
-        console.log(data)
-        reset()
-   }
+   const conversationId = useRef(crypto.randomUUID());
 
-const onKeyDown = (e:React.KeyboardEvent<HTMLFormElement>) => {
-    if(e.key === 'Enter' && !e.shiftKey) {
-                e.preventDefault();
-                handleSubmit(onSubmit)();
-            }
-}
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
+   const onSubmit = async ({ prompt }: FormData) => {
+      reset();
+      const {data} = await axios.post('/api/chat', {
+         prompt,
+         conversationId: conversationId.current
+      });
+      console.log(data);
+   };
+
+   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         e.preventDefault();
+         handleSubmit(onSubmit)();
+      }
+   };
 
    return (
       <form
@@ -27,18 +35,18 @@ const onKeyDown = (e:React.KeyboardEvent<HTMLFormElement>) => {
          className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
       >
          <textarea
-            {...register('prompt', 
-                { 
-                required: true,
-                validate: data => data.trim().length > 0
+            {...register('prompt', {
+               required: true,
+               validate: (data) => data.trim().length > 0,
             })}
             className="w-full border-0 focus:outline-0 resize-none"
             placeholder="Ask anything"
             maxLength={1000}
          />
-         <Button 
-         disabled={!formState.isValid}
-         className="rounded-full w-9 h-9 cursor-pointer">
+         <Button
+            disabled={!formState.isValid}
+            className="rounded-full w-9 h-9 cursor-pointer"
+         >
             <FaArrowUp />
          </Button>
       </form>
