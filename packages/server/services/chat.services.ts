3b2345df4ef20d957a3de5e5f6116540100715db
Dmitriy <mr.dima337@gmail.com>
2025-10-18 23:47:10 +0300

    feat: customise chatbot. add folders prompts

--- packages/server/services/chat.services.ts
+++ packages/server/services/chat.services.ts
@@ -1,22 +1,35 @@
-import OpenAI from "openai";
-import { conversationRepository } from "../repositories/conversation.repository";
+import fs from 'fs';
+import path from 'path';
+import OpenAI from 'openai';
+import { conversationRepository } from '../repositories/conversation.repository';
+import template from '../prompts/chatbot.txt';
 
 // Implementation detail
 const client = new OpenAI({
-apiKey: process.env.OPEN_API_KEY,
+   apiKey: process.env.OPEN_API_KEY,
 });
 
+const parkInfo = fs.readFileSync(
+   path.join(__dirname, '..', 'prompts', 'WonderWorld.md'),
+   'utf-8'
+);
+const instructions = template.replace('{{parkInfo}}', parkInfo);
+
 type ChatResponse = {
-    id: string;
-    message: string;
-}
+   id: string;
+   message: string;
+};
 
 // Public interface
 // Leaky abstraction
 export const chatService = {
-    async sendMessage(prompt: string, conversationId: string): Promise<ChatResponse> {
-        const response = await client.responses.create({
+   async sendMessage(
+      prompt: string,
+      conversationId: string
+   ): Promise<ChatResponse> {
+      const response = await client.responses.create({
          model: 'gpt-4o-mini',
+         instructions,
          input: prompt,
          temperature: 0.2,
          max_output_tokens: 500,
@@ -26,9 +39,9 @@ export const chatService = {
 
       conversationRepository.setLastResponseId(conversationId, response.id);
 
-      return  {
-        id: response.id,
-        message: response.output_text
-      }
-    }
-}
+      return {
+         id: response.id,
+         message: response.output_text,
+      };
+   },
+};
