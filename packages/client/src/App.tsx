0c1288354236e8b38fc8f2e684a259bc3bac6a58
Dmitriy <mr.dima337@gmail.com>
2025-12-22 23:39:41 +0300

    refactor: change margin

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,7 +2,7 @@ import ChatBot from './components/chat/ChatBot';
 
 function App() {
    return (
-      <div className="p-4 h-screen w-full">
+      <div className="p-4 h-screen w-full max-w-[1000px] mx-auto pt-20">
          <ChatBot />
       </div>
    );
